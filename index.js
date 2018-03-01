2fc401bf0154206baa536314c0369e21424c3a43
Morgan Fudge <mfudge92@gmail.com>
2018-03-01 15:01:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,5 @@ let newID = 1000000000 + oldID;
 let ageIsValid = Number.isInteger(currentAge);
 
 let randomNumber = Math.floor(Math.random() * 20);
+
+let randomInteger = Math.floor(Math.random() * 20);
